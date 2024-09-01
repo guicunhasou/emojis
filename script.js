bcc790807128d43c74d41dbb116cf54927f10629
Guilherme Cunha <guicunhasou@gmail.com>
2024-09-01 06:03:02 -0300

    v.1.1.1 navegação via teclado & favicon

--- script.js
+++ script.js
@@ -33,3 +33,19 @@ document.getElementById('anteriorBoca').addEventListener('click', function() {
   numeroImgBocaAtual = numeroImgBocaAtual > 1 ? numeroImgBocaAtual - 1 : totalImagens;
   trocarImagemBoca();
 });
+
+document.addEventListener('keydown', function(event) {
+  if (event.key.toLowerCase() === 'o') {
+      numeroImgOlhosAtual = numeroImgOlhosAtual < totalImagens ? numeroImgOlhosAtual + 1 : 1;
+      trocarImagemOlhos();
+  } else if (event.key.toLowerCase() === 'i') {
+      numeroImgOlhosAtual = numeroImgOlhosAtual > 1 ? numeroImgOlhosAtual - 1 : totalImagens;
+      trocarImagemOlhos();
+  } else if (event.key.toLowerCase() === 'b') {
+      numeroImgBocaAtual = numeroImgBocaAtual < totalImagens ? numeroImgBocaAtual + 1 : 1;
+      trocarImagemBoca();
+  } else if (event.key.toLowerCase() === 'v') {
+      numeroImgBocaAtual = numeroImgBocaAtual > 1 ? numeroImgBocaAtual - 1 : totalImagens;
+      trocarImagemBoca();
+  }
+});
